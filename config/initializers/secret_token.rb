31900e3fba4e6753c2739335f90cb0abfe3af566
Keitaroh Kobayashi <keita@kkob.us>
2013-12-05 09:47:12 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WpcampApi::Application.config.secret_key_base = 'a3d29aa867b529978bac54ca20b2a554094cb98384b402aeefb2d019a14cc4acb7e68fa73d3137424b71aa4ab10d9d8b4605ef523c3ec3ba1055f9bfb039e7ce'
