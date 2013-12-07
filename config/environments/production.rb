0848ea86049ea63bbe53cbb0e2ea2cc3683f7c04
Keitaroh Kobayashi <keita@kkob.us>
2013-12-07 20:31:38 +0900

    Use HTTPS for all asset requests

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ WpcampApi::Application.configure do
   config.cache_store = :dalli_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "//s3-#{ENV['FOG_REGION']}.amazonaws.com/#{ENV['FOG_DIRECTORY']}"
+  config.action_controller.asset_host = "https://s3-#{ENV['FOG_REGION']}.amazonaws.com/#{ENV['FOG_DIRECTORY']}"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
