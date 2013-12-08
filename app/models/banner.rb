89ecf8508ee0f3d3cd6cb004df9de19f1b57dcf4
Keitaroh Kobayashi <keita@kkob.us>
2013-12-08 21:06:44 +0900

    set the width and height on upload, but only if they aren't filled in

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -14,4 +14,14 @@ class Banner < ActiveRecord::Base
     content_type: { content_type: ["image/jpg", "image/jpeg", "image/pjpeg", "image/gif", "image/png"] },
     size: { in: 0..5.megabytes }
 
+  after_post_process :save_banner_dimensions
+
+  private
+
+    def save_banner_dimensions
+      geo = Paperclip::Geometry.from_file(banner.queued_for_write[:original])
+      self.width = geo.width.to_i unless self.width and self.width > 0
+      self.height = geo.height.to_i unless self.height and self.height > 0
+    end
+
 end
